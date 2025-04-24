42f774aed6eb7daf2e25171a31ebe78dfe8fa702
Christian Einhardt <47864356+chrisesf@users.noreply.github.com>
2025-04-24 15:30:31 -0300

    Update plotly_panel.R

--- plotly_panel.R
+++ plotly_panel.R
@@ -1,50 +1,66 @@
 library(plotly)
 library(tidyr)
 library(dplyr)
+library(scales)
 
-panel_st_plotly_native_v1 <- function(data){
-  # build rectangle points
+# Função para criar o gráfico Plotly com customdata para o JobId
+panel_st_plotly_native_v1 <- function(data) {
   plotly_raw_data <- data$Application %>%
     arrange(End) %>%
     select(Start, End, Position, Height, Value, JobId) %>%
-    mutate(xP0 = Start, yP0 = Position,                 # 1st point of rectangle
-           xP1 = End,   yP1 = Position,                 # 2nd point
-           xP2 = End,   yP2 = Position + Height - 0.2,  # 3rd point
-           xP3 = Start, yP3 = Position + Height - 0.2,  # 4th point
-           xP4 = Start, yP4 = Position,                 # 5st point (going back to origin, i.e. P0)
-           xP5 = NA,    yP5 = NA                        # NA to signal the end of this polygon
+    mutate(
+      xP0 = Start, yP0 = Position,
+      xP1 = End,   yP1 = Position,
+      xP2 = End,   yP2 = Position + Height - 0.2,
+      xP3 = Start, yP3 = Position + Height - 0.2,
+      xP4 = Start, yP4 = Position,
+      xP5 = NA,    yP5 = NA
     ) %>%
     select(-Start, -End, -Position, -Height)
   
-  # list of x and y pairs
-  plotly_raw_data %>%
+  plotly_data_xy <- plotly_raw_data %>%
     pivot_longer(
       cols = starts_with("x") | starts_with("y"),
       names_to = c(".value", "group"),
-      names_pattern = "(.)P(\\d)") -> plotly_data_xy
+      names_pattern = "(.)P(\\d)"
+    )
   
-  # merge with color info
-  plotly_data_xy %>%
-    left_join(data$Colors %>% select(Value, Color), by = "Value") ->
-    plotly_data
+  plotly_data <- plotly_data_xy %>%
+    left_join(data$Colors %>% select(Value, Color), by = "Value")
   
-  # plot
-  plot_ly(data = plotly_data,
-          x = ~x,
-          y = ~y,
-          mode="lines",
-          fill="toself",
-          text=~paste("Value:", Value, "<br>JobId:", JobId),
-          hoveron="points",
-          hoverinfo="text",
-          type="scatter",
-          split=~Value,
-          color = I(plotly_data$Color))
+  p <- plot_ly(
+    data = plotly_data,
+    x = ~x,
+    y = ~y,
+    mode = "lines",
+    fill = "toself",
+    text = ~paste("Value:", Value, "<br>JobId:", JobId),
+    customdata = ~JobId,
+    hoveron = "points+fills", 
+    hoverinfo = "text",
+    type = "scatter",
+    split = ~Value,
+    color = I(plotly_data$Color)
+  )
+  
+  p <- p %>% layout(
+    xaxis = list(
+      rangeslider = list(
+        visible = TRUE 
+      ),
+      range = c(data$config$limits$start, data$config$limits$end)
+    ),
+    width = 1200,  
+    height = 600,  
+    hovermode = "closest",
+    dragmode = "zoom" 
+  )
+  
+  return(p)
 }
 
 
 panel_st_plotly_native_v2 <- function(data){
-  # build rectangle points
   plotly_raw_data <- data$Application %>%
     arrange(End) %>%
     select(Start, End, Position, Height, Value, JobId) %>%
@@ -57,14 +73,12 @@ panel_st_plotly_native_v2 <- function(data){
     ) %>%
     select(-Start, -End, -Position, -Height)
   
-  # list of x and y pairs
   plotly_raw_data %>%
     pivot_longer(
       cols = starts_with("x") | starts_with("y"),
       names_to = c(".value", "group"),
       names_pattern = "(.)P(\\d)") -> plotly_data_xy
   
-  # merge with color info
   plotly_data_xy %>%
     left_join(data$Colors %>% select(Value, Color), by = "Value") ->
     plotly_data
@@ -74,16 +88,16 @@ panel_st_plotly_native_v2 <- function(data){
                       y=ResourceId,
                       fill=Value)) +
              #scale_y_reverse() +
-             theme_bw()) %>% # I dont know how to do a empty plotequivalent to that without ggplotly
-  add_trace(data = plotly_data,
-            x = ~x,
-            y = ~y,
-            mode="lines",
-            fill="toself",
-            text=~paste("Value:", Value, "<br>JobId:", JobId),
-            hoveron="points",
-            hoverinfo="text",
-            type="scatter",
-            split=~Value,
-            color = I(plotly_data$Color))
+             theme_bw()) %>%
+    add_trace(data = plotly_data,
+              x = ~x,
+              y = ~y,
+              mode="lines",
+              fill="toself",
+              text=~paste("Value:", Value, "<br>JobId:", JobId),
+              hoveron="points",
+              hoverinfo="text",
+              type="scatter",
+              split=~Value,
+              color = I(plotly_data$Color))
 }

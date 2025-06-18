02ff04a41a4f12c738bedf4f577e19775ad4084f
Christian Einhardt <47864356+chrisesf@users.noreply.github.com>
2025-06-18 14:46:38 -0300

    Create starvz-tikki-phase1.R

---
+++ starvz-tikki-phase1.R
@@ -0,0 +1,169 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)==0) {
+  trace_dir = getwd()
+} else if (length(args)==1) {
+  trace_dir = args[1]
+}
+
+library(readr)
+library(dplyr, warn.conflicts = FALSE)
+options(crayon.enabled = FALSE)
+library(tidyr)
+suppressMessages(library(RColorBrewer))
+
+phase1 <- function(direct){
+  file1 <- paste(direct, "tasks.csv", sep="/")
+
+  suppressWarnings(
+    classes = "vroom_parse_issue", # tasks.csv header lists an additional column
+    df1 <- read_delim(file1, delim=",", col_names=TRUE,
+                      col_types = cols(Resource = col_double(),
+                                       Numa = col_integer(),
+                                       Start = col_double(),
+                                       End = col_double(),
+                                       Duration = col_double(),
+                                       Explicit = col_integer(),
+                                       Aff = col_integer(),
+                                       Strict = col_integer(),
+                                       Tag = col_character(),
+                                       Key0 = col_character(),
+                                       Key1 = col_character(),
+                                       Name = col_character(),
+                                       TaskId = col_integer(),
+                                       ParentTaskId = col_integer(),
+                                       Work = col_character()))
+    )
+    
+  df1 <- df1 %>% arrange(TaskId) %>% mutate(JobId = 1) %>% mutate(JobId = cumsum(JobId)-1)
+
+  data <- df1 %>%
+    rename(Value = Name) %>%
+    mutate(Nature = "State", ResourceId = paste0("CPU", Resource),
+           Type = "Worker State",
+           Params = NA, Footprint = NA,
+           SubmitOrder = NA,
+           GFlop = NA,
+           Depth = NA,
+           Size = NA,
+           Iteration = NA,
+           X = NA,
+           Y = NA,
+           Subiteration = NA) %>%
+    select(-Resource, -Work, - Key0, -Key1,
+           -Numa,
+    ) %>%
+    mutate(End = End/1000000,
+           Start = Start/1000000,
+           Duration = Duration/1000000) %>%
+    select(Nature, ResourceId, Type, Start, End, Duration, Depth, Value, Size, Params, Footprint, Tag, JobId, SubmitOrder, GFlop, X, Y, Iteration, Subiteration)
+
+  write_csv(data, paste(direct, "paje.worker_state.csv.gz", sep="/"), quote = "none")
+
+  # file2 <- paste(direct, "dag.csv", sep="/")
+  # df2 <- read_delim(file2, delim=",", col_names=TRUE,
+  #                   col_types = cols(Dependent = col_character(),
+  #                                    JobId = col_character()))
+
+  dag_rec_raw <- read_csv(paste(direct, "dag-do-rec-raw.csv", sep="/"),
+                          col_types = cols(Name = col_factor(),
+                                           JobId = col_character(),
+                                           DependsOn = col_character())) %>%
+    mutate(Value = factor(Name, levels = c("gemm", "potrf", "syrk", "trsm"))) %>%
+    select(-Name) %>%
+    separate_rows(DependsOn, sep = " ")
+
+  dirdagrec <- paste(direct, "rec-dag/", sep="/")
+  dir.create(dirdagrec, showWarnings = FALSE)
+
+  dag_rec_raw %>%
+    select(JobId, DependsOn) %>%
+    rename(Dependent = DependsOn) %>%
+    select(Dependent, JobId) %>%
+    na.omit() %>%
+    write_csv(paste0(dirdagrec, "dag.csv.gz"), quote = "none")
+
+}
+
+
+phase1_recursive <- function(direct){
+  file1 <- paste(direct, "tasks.csv", sep="/")
+  suppressWarnings(
+    classes = "vroom_parse_issue", # tasks.csv header lists an additional column
+    df1 <- read_delim(file1, delim=",", col_names=TRUE,
+                      col_types = cols(Resource = col_double(),
+                                       Numa = col_integer(),
+                                       Start = col_double(),
+                                       End = col_double(),
+                                       Duration = col_double(),
+                                       Explicit = col_integer(),
+                                       Aff = col_integer(),
+                                       Strict = col_integer(),
+                                       Tag = col_character(),
+                                       Key0 = col_character(),
+                                       Key1 = col_character(),
+                                       Name = col_character(),
+                                       TaskId = col_integer(),
+                                       ParentTaskId = col_integer(),
+                                       Work = col_character()))
+    )
+
+  # considering unique TaskId
+  tmp_JobId <- df1 %>% select(TaskId) %>% unique %>% arrange(TaskId) %>% mutate(JobId = 1) %>% mutate(JobId = cumsum(JobId)-1)
+  df1 <- df1 %>% left_join(tmp_JobId)
+
+  # tentando ficar só com as folhas nos casos recursivos
+  hasChildren <- unique(df1$ParentTaskId)
+  df1 <- df1 %>% filter(!(TaskId %in% hasChildren))
+
+  data <- df1 %>%
+    rename(Value = Name) %>%
+    mutate(Nature = "State", ResourceId = paste0("CPU", Resource),
+           Type = "Worker State",
+           Params = NA, Footprint = NA,
+           SubmitOrder = NA,
+           GFlop = NA,
+           Depth = NA,
+           Size = NA,
+           Iteration = NA,
+           X = NA,
+           Y = NA,
+           Subiteration = NA) %>%
+    select(-Resource, -Work, - Key0, -Key1,
+           -Numa,
+    ) %>%
+    mutate(End = End/1000000,
+           Start = Start/1000000,
+           Duration = Duration/1000000) %>%
+    select(Nature, ResourceId, Type, Start, End, Duration, Depth, Value, Size, Params, Footprint, Tag, JobId, SubmitOrder, GFlop, X, Y, Iteration, Subiteration) %>%
+    unique
+
+  write_csv(data, paste(direct, "paje.worker_state.csv.gz", sep="/"), quote = "none")
+
+  # file2 <- paste(direct, "dag.csv", sep="/")
+  # df2 <- read_delim(file2, delim=",", col_names=TRUE,
+  #                   col_types = cols(Dependent = col_character(),
+  #                                    JobId = col_character()))
+
+  dag_rec_raw <- read_csv(paste(direct, "dag-do-rec-raw.csv", sep="/"),
+                          col_types = cols(Name = col_factor(),
+                                           JobId = col_character(),
+                                           DependsOn = col_character())) %>%
+    mutate(Value = factor(Name, levels = c("gemm", "potrf", "syrk", "trsm"))) %>%
+    select(-Name) %>%
+    separate_rows(DependsOn, sep = " ")
+
+  dirdagrec <- paste(direct, "rec-dag/", sep="/")
+  dir.create(dirdagrec, showWarnings = FALSE)
+
+  dag_rec_raw %>%
+    select(JobId, DependsOn) %>%
+    rename(Dependent = DependsOn) %>%
+    select(Dependent, JobId) %>%
+    na.omit() %>%
+    write_csv(paste0(dirdagrec, "dag.csv.gz"), quote = "none")
+
+}
+
+phase1(trace_dir)

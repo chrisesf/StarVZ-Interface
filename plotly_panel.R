02a69d47eae824c53cced379d519b318cd734385
Christian Einhardt <47864356+chrisesf@users.noreply.github.com>
2025-07-06 17:16:55 -0300

    Update plotly_panel.R

--- plotly_panel.R
+++ plotly_panel.R
@@ -3,7 +3,7 @@ library(tidyr)
 library(dplyr)
 library(scales)
 
-# Função para criar o gráfico Plotly com customdata para o JobId
+# plotly_panel.R (Your existing code for v1)
 panel_st_plotly_native_v1 <- function(data) {
   plotly_raw_data <- data$Application %>%
     arrange(End) %>%
@@ -17,17 +17,24 @@ panel_st_plotly_native_v1 <- function(data) {
       xP5 = NA,    yP5 = NA
     ) %>%
     select(-Start, -End, -Position, -Height)
-  
+
   plotly_data_xy <- plotly_raw_data %>%
     pivot_longer(
       cols = starts_with("x") | starts_with("y"),
       names_to = c(".value", "group"),
       names_pattern = "(.)P(\\d)"
     )
-  
+
   plotly_data <- plotly_data_xy %>%
     left_join(data$Colors %>% select(Value, Color), by = "Value")
-  
+
+  # --- ADD THIS DEBUGGING LINE ---
+  print("Head of plotly_data:")
+  print(head(plotly_data))
+  print("Summary of JobId column:")
+  print(summary(plotly_data$JobId))
+  # --- END DEBUGGING LINES ---
+
   p <- plot_ly(
     data = plotly_data,
     x = ~x,
@@ -36,29 +43,28 @@ panel_st_plotly_native_v1 <- function(data) {
     fill = "toself",
     text = ~paste("Value:", Value, "<br>JobId:", JobId),
     customdata = ~JobId,
-    hoveron = "points+fills", 
+    hoveron = "points+fills",
     hoverinfo = "text",
     type = "scatter",
     split = ~Value,
     color = I(plotly_data$Color)
   )
-  
+
   p <- p %>% layout(
     xaxis = list(
       rangeslider = list(
-        visible = TRUE 
+        visible = TRUE
       ),
       range = c(data$config$limits$start, data$config$limits$end)
     ),
-    width = 1200,  
-    height = 600,  
+    width = 1200,
+    height = 600,
     hovermode = "closest",
-    dragmode = "zoom" 
+    dragmode = "zoom"
   )
-  
+
   return(p)
 }
-
 
 panel_st_plotly_native_v2 <- function(data){
   plotly_raw_data <- data$Application %>%
